fe8e901d6ab0384c8630f852bf6b8049fbac573f
Eric <ericcui@qq.com>
2024-06-08 20:52:18 +0800

    add code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object? sender, EventArgs e)
+        {
+            var win2 = new Window(new SecondPage());
+            Application.Current?.OpenWindow(win2);
+
+            var win3 = new Window(new SecondPage());
+            Application.Current?.OpenWindow(win3);
+
+            var win4 = new Window(new SecondPage());
+            Application.Current?.OpenWindow(win4);
+
+            var win5 = new Window(new SecondPage());
+            Application.Current?.OpenWindow(win5);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Application.Current?.Quit();
+        }
+    }
+
+}
